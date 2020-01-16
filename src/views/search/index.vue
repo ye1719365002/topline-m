ac40fbf30b1344e971a3ba8125794840657c56e6
ye1719365002 <1719365002@qq.com>
2020-01-16 10:05:15 +0800

    <style scoped lang=less>

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="search-container">
     <!-- 搜索栏 -->
-    <form action="/">
+    <form class="search-form" action="/">
       <van-search
         v-model="searchText"
         placeholder="请输入搜索关键词"
         show-action
         @search="onSearch"
         @cancel="$router.back()"
+        @focus="isResultShow = false"
       />
     </form>
     <!-- /搜索栏 -->
+     <!-- 搜索结果 -->
+    <search-result v-if="isResultShow" />
+    <!-- /搜索结果 -->
 
     <!-- 联想建议 -->
-    <van-cell-group>
+    <van-cell-group v-else-if="searchText">
       <van-cell title="单元格" icon="search" />
       <van-cell title="单元格" icon="search" />
       <van-cell title="单元格" icon="search" />
@@ -24,7 +28,7 @@
     <!-- /联想建议 -->
 
     <!-- 历史记录 -->
-    <van-cell-group>
+    <van-cell-group v-else>
       <van-cell title="历史记录">
         <van-icon name="delete"></van-icon>
         <span>全部删除</span>
@@ -47,7 +51,7 @@
     <!-- /历史记录 -->
 
     <!-- 搜索结果 -->
-    <search-result />
+    <!-- <search-result /> -->
     <!-- /搜索结果 -->
   </div>
 </template>
@@ -62,7 +66,8 @@ export default {
   props: {},
   data () {
     return {
-      searchText: ''
+      searchText: '',
+      isResultShow: false
     }
   },
   computed: {},
@@ -72,9 +77,21 @@ export default {
   methods: {
     onSearch () {
       console.log('onSearch')
+      this.isResultShow = true
     }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.search-container {
+  padding-top: 54px;
+  .search-form {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+  }
+}
+</style>
